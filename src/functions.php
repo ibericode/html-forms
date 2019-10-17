6c986aab600ec1a769b1d85dece58b294eb009de
hybridvision <hybridvision@users.noreply.github.com>
2019-10-17 12:56:29 +0200

    Only convert to HTML entities where necessary.

--- src/functions.php
+++ src/functions.php
@@ -223,14 +223,16 @@ function hf_template( $template ) {
 /**
  * @param string $string
  * @param array $data
+ * @param mixed $escape_function
+ * @param bool $convert_html_entities
  *
  * @return string
  */
-function hf_replace_data_variables($string, $data = array(), $escape_function = null) {
-    $string = preg_replace_callback( '/\[([a-zA-Z0-9\-\._]+)\]/', function($matches) use ($data, $escape_function) {
+function hf_replace_data_variables($string, $data = array(), $escape_function = null, $convert_html_entities = false) {
+    $string = preg_replace_callback( '/\[([a-zA-Z0-9\-\._]+)\]/', function($matches) use ($data, $escape_function, $convert_html_entities) {
         $key = $matches[1];
         $replacement = hf_array_get( $data, $key, '' );
-        $replacement = hf_field_value( $replacement );
+        $replacement = hf_field_value( $replacement, 0, $convert_html_entities );
         if ($escape_function !== null && is_callable($escape_function)) {
             $replacement = $escape_function($replacement);
         }
@@ -245,11 +247,12 @@ function hf_replace_data_variables($string, $data = array(), $escape_function = 
 * Caveat: if value is a file, an HTML string is returned (which means email action should use "Content-Type: html" when it includes a file field).
 *
 * @param string|array $value
-* @param int $limit 
+* @param int $limit
+* @param bool $convert_html_entities
 * @return string
 * @since 1.3.1
 */
-function hf_field_value( $value, $limit = 0 ) {
+function hf_field_value( $value, $limit = 0, $convert_html_entities = true ) {
     if( $value === '' ) {
         return $value;
     }
@@ -286,7 +289,11 @@ function hf_field_value( $value, $limit = 0 ) {
 
     // escape
     $value = wp_check_invalid_utf8($value);
-    $value = htmlentities($value, ENT_NOQUOTES);
+
+    if ($convert_html_entities) {
+        $value = htmlentities($value, ENT_NOQUOTES);
+    }
+
     return $value;
 }
 
