1d7e000e43462ce102cb2131b6295ba6b4f69827
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2021-05-11 10:54:05 +0200

    fix eslint warnings

--- assets/src/js/form-loading-indicator.js
+++ assets/src/js/form-loading-indicator.js
@@ -17,34 +17,34 @@ function Loader (formEl, char) {
 
   function start () {
     if (button) {
-	  const loadingText = button.getAttribute('data-loading-text')
-	  if (loadingText) {
+      const loadingText = button.getAttribute('data-loading-text')
+      if (loadingText) {
         // Use text from "data-loading-text" attribute
         setButtonText(button, loadingText)
-	  } else {
+      } else {
         // Use default loading character (. .. ...)
         button.style.width = window.getComputedStyle(button).width
         setButtonText(button, char)
         loadingInterval = window.setInterval(tick, 500)
-	  }
+      }
     } else {
-	  // If form had no button element, change opacity
-	  formEl.style.opacity = '0.5'
+      // If form had no button element, change opacity
+      formEl.style.opacity = '0.5'
     }
 
     if (formEl.classList) {
-	  formEl.classList.add('mc4wp-loading')
+      formEl.classList.add('mc4wp-loading')
     }
   }
 
   function stop () {
     if (button) {
-	  button.style.width = originalButton.style.width
-	  const text = getButtonText(originalButton)
-	  setButtonText(button, text)
-	  window.clearInterval(loadingInterval)
+      button.style.width = originalButton.style.width
+      const text = getButtonText(originalButton)
+      setButtonText(button, text)
+      window.clearInterval(loadingInterval)
     } else {
-	  formEl.style.opacity = ''
+      formEl.style.opacity = ''
     }
 
     formEl.className = formEl.className.replace('mc4wp-loading', '')
@@ -55,7 +55,10 @@ function Loader (formEl, char) {
     setButtonText(button, text.length >= 5 ? char : text + ' ' + char)
   }
 
-  return { start, stop }
+  return {
+    start,
+    stop
+  }
 }
 
 module.exports = Loader
