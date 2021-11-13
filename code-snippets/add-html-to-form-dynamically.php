2b6c573acb3f362c9b8f7d9f803d7a0ff08655d2
Arne Lap <arnelap@users.noreply.github.com>
2021-11-13 19:35:23 +0700

    Update add-html-to-form-dynamically.php

--- code-snippets/add-html-to-form-dynamically.php
+++ code-snippets/add-html-to-form-dynamically.php
@@ -7,3 +7,10 @@ add_filter(
 		return $markup;
 	}
 );
+
+
+// HTML forms will check the number of configured fields versus the number of submitted fields, and ignore the submission if it's mismatching. 
+// For that reason, if you like to dynamically add more than 1 field dynamically, you need to disable he field counter with the following filter:
+
+add_filter('hf_validate_form_request_size', false);
+
