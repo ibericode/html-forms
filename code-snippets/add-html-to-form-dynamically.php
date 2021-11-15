da275969c99c66abe6c2fa4b3b097505b6ade0b5
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2021-11-15 08:49:44 +0100

    fix phpcs

--- code-snippets/add-html-to-form-dynamically.php
+++ code-snippets/add-html-to-form-dynamically.php
@@ -9,8 +9,8 @@ add_filter(
 );
 
 
-// HTML forms will check the number of configured fields versus the number of submitted fields, and ignore the submission if it's mismatching. 
+// HTML forms will check the number of configured fields versus the number of submitted fields, and ignore the submission if it's mismatching.
 // For that reason, if you like to dynamically add more than 1 field dynamically, you need to disable he field counter with the following filter:
 
-add_filter('hf_validate_form_request_size', false);
+add_filter( 'hf_validate_form_request_size', false );
 
