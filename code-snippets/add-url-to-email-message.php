684e143a9d3970e374e40f9dce7cc59600ca1cc6
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-09-23 09:43:47 +0200

    fix cs

--- code-snippets/add-url-to-email-message.php
+++ code-snippets/add-url-to-email-message.php
@@ -1,8 +1,11 @@
-<?php 
+<?php
 /*
-* It's possible to filter the email action message with PHP. 
-* This example will add the URL the form was submitted from to the end of the email notificationmessage. 
+* It's possible to filter the email action message with PHP.
+* This example will add the URL the form was submitted from to the end of the email notification message.
 */
-add_filter( 'hf_action_email_message', function( $message ) {
-return $message."<br>Referer URL: ".$_SERVER['REQUEST_URI'];
-});
+add_filter(
+	'hf_action_email_message',
+	function ( $message ) {
+		return sprintf( '%s <br />Referrer URL: %s', $message, $_SERVER['REQUEST_URI'] );
+	}
+);
