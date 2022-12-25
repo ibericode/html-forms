bd2b72cf95f0416b74e545952a5f5aa65d5c61a0
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-12-25 16:47:56 +0100

    use webpack for everything instead of gulp

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const postcss = require('postcss');
+const cssnano = require('cssnano');
+
+module.exports = {
+  entry: {
+    public: './assets/src/js/public.js',
+    'gutenberg-block': './assets/src/js/gutenberg-block.js',
+    admin: './assets/src/js/admin/admin.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'assets/js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: [/\/node_modules\//],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env'],
+            ],
+            plugins: [
+              [
+                '@babel/plugin-transform-react-jsx',
+                {
+                  pragma: 'h',
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: './assets/src/img', to: path.resolve(__dirname, './assets/img') },
+        {
+          from: './assets/src/css',
+          to: path.resolve(__dirname, './assets/css'),
+          transform: (content, path) => postcss([cssnano])
+            .process(content, {
+              from: path,
+            })
+            .then((result) => result.css),
+        },
+      ],
+    }),
+  ],
+};
